15dfbaa229d36efa691e16b7f51e9ab51374486e
Hamdi Kandil <hamdikendil.insatgmail.com>
2025-05-10 17:31:09 +0100

    feat: updated API url

--- API/index.mjs
+++ API/index.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 import serverless from "serverless-http";
 import cors from "cors";
-import { fetchTasks, createTasks, updateTasks, deleteTasks } from "./task.js";
+import { fetchTasks, createTasks, updateTasks, deleteTasks } from "./task.mjs";
 
 const app = express();
 const port = 3001;
